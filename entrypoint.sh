543d1d5e12d361975e069a7ba434806f68a47e86
Andrei Luca <andrei.luca@eventya.net>
2024-12-03 09:38:19 +0200

    Update ENV passed to cronjob

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+# Exit on any error
+set -e
+
+# Export all environment variables to a file for the cron job
+env > /scripts/env
 
 # Write the cron job schedule to a file
-echo "$CRON_SCHEDULE /scripts/backup.sh >> /var/log/cron/backup.log 2>&1" > /etc/cron.d/postgres-backup
+echo "$CRON_SCHEDULE /scripts/env && /scripts/backup.sh >> /var/log/cron/backup.log 2>&1" > /etc/cron.d/postgres-backup
 
 # Give execution rights on the cron job
 chmod 0644 /etc/cron.d/postgres-backup
